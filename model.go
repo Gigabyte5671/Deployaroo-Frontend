dac25944591a7be7d5dc7439b9afddb3bd61b3d7
Giga <gigabyte5671@gmail.com>
2024-03-08 08:38:31 +1300

    Create model initialisers

--- model.go
+++ model.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/charmbracelet/bubbles/spinner"
 	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+	lg "github.com/charmbracelet/lipgloss"
 )
 
 type deployment struct {
@@ -36,3 +38,29 @@ const (
 	ErrorView
 )
 
+func InitialModel() model {
+	ti := textinput.New()
+	ti.Placeholder = ""
+	ti.Focus()
+	ti.CharLimit = 0
+	ti.Width = 70
+
+	s := spinner.New()
+	s.Spinner = spinner.Line
+	s.Style = lg.NewStyle().Foreground(lg.Color("205"))
+
+	return model {
+		deployments: []deployment{},
+		history: []string{"", "", "What would you like to do?"},
+		textInput: ti,
+		spinner: s,
+		boxWidth: 30,
+		loadingDeployments: false,
+		loadingResponse: false,
+		view: DefaultView,
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return textinput.Blink
+}
